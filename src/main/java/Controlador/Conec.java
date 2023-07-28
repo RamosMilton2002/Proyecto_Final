2959911e5528b0da4d66a58cd89f0bd43a3cae28
MiltonRamos2002 <milton.ramos954@ist17dejulio.edu.ec>
2023-07-28 10:04:16 -0500

    Modleo vista controlador

--- src/main/java/Controlador/Conec.java
+++ src/main/java/Controlador/Conec.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Conexion;
+package Controlador;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
